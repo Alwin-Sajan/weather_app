54b479efa5f6c693695ed88e9ae9ce632096523d
Alwin-Sajan <alwinsajan225@gmail.com>
2024-07-16 18:59:02 +0530

    updated multi bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+// import {bg1,bg2,bg3,bg4,bg5} from './assets/index.js'
+import './assets/bg1.jpg' 
 
 const App = () => {
+
+  // const [data,setData] = useState([])
+
+  
+  const [bg ,setBg] = useState('bg-blue-400')
 
   const data ={
     place : 'London',
@@ -16,25 +23,33 @@ const App = () => {
 
 
   return (
-    <div className='w-full h-auto relative bg-blue-300 pb-8'>
+    <div className={`w-full h-auto relative ${bg} pb-8`}>
       <h1 className='text-white p-4 absolute font-bold'>Weather App</h1>
       
       <div className='w-full h-full pt-28 flex items-center justify-center'>
         <div className='w-[68vw] h-[66vh] bg-white rounded-lg shadow-2xl p-8'>
 
           <h1 className='rounded-2xl p-1 text-2xl text-center'>{data.place}</h1>
-          <div className='grid justify-center items-center m-2 p-4 bg-blue-100'>
-            <h2 className=''>{data.date}</h2>
-            <h1 className='text-7xl font-semibold m-8'>{data.curr}°c</h1>
+          <div className='flex justify-center items-center m-2 p-4 bg-blue-100'>
+            <div className='bg-blue-300 p-8'>
+            <h2 className='p-2'>{data.date}</h2>
             <h2>{data.max}°c / {data.min}°c</h2>
+            
+            </div>
+            <h1 className='text-7xl font-semibold border-4 rounded-lg border-sky-400 p-8 m-8'>{data.curr}°c</h1>
+            
           </div>
           <h3 className=' float-right mr-2'>Time : {data.time}</h3>
         </div>
       </div>
 
-      <div className='w-1/3 h-12 bg-slate-400 mt-6 mx-auto '>
-        <button className='w-12 h-12 bg-orange-500'>blue</button>
-        
+      <div className='w-1/3 h-12 flex justify-between shadow-2xl bg-slate-200 mt-6 mx-auto rounded-2xl px-6'>
+        <button className='w-16 h-12 bg-red-500' onClick={()=>{setBg('bg-red-500')}}></button>
+        <button className='w-16 h-12 bg-orange-500 ' onClick={()=>{setBg('bg-orange-400')}}></button> 
+        <button className='w-16 h-12 bg-yellow-500 ' onClick={()=>{setBg('bg-yellow-400')}}></button> 
+        <button className='w-16 h-12 bg-green-400 ' onClick={()=>{setBg('bg-green-400')}}></button> 
+        <button className='w-16 h-12 bg-blue-400 ' onClick={()=>{setBg('bg-blue-400')}}></button> 
+
       </div>
 
     </div>
